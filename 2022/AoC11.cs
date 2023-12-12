ee74951595e2e1f9e3be16adbad66ab36ed3ded2
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2023-12-11 20:59:13 -0800

    22 Day 11 Part 2 Solved
    Had to use help from reddit about the math to reduce without effecting the monkey calc

--- 2022/AoC11.cs
+++ 2022/AoC11.cs
@@ -25,18 +25,24 @@ namespace CodeTAF
             public int FalseMNum { get; }
             private Monkey ThrowToTrue { get; set; }
             private Monkey ThrowToFalse { get; set; }  
-            public Func<ulong, ulong> Operation { get; set; }
-            private List<ulong> Items { get; set; }
+            public Func<long, long> Operation { get; set; }
+            private List<long> Items { get; set; }
 
             public int Inspects { get; set; }
 
+            const long MAX_NUM = 1000000;            
 
-            public Monkey(int testDiv,int trueMNum,int falseMNum, List<ulong> items) {
+            static int BigDiv = 1;
+
+
+            public Monkey(int testDiv,int trueMNum,int falseMNum, List<long> items) {
                 TestDiv = testDiv;
                 Items = items;
                 TrueMNum = trueMNum;
                 FalseMNum = falseMNum;
                 Inspects = 0;
+                BigDiv *= testDiv;
+                print(BigDiv);
             }            
 
             public void SetThrow(Monkey mTrue, Monkey mFalse) {
@@ -44,16 +50,24 @@ namespace CodeTAF
                 ThrowToFalse = mFalse;
             }
 
-            void Catch(ulong item) {
+            void Catch(long item) {
                 Items.Add(item);
             }
 
-            void ThrowTo(Monkey targetM, ulong item) {
+            void ThrowTo(Monkey targetM, long item) {
                 targetM.Catch(item);
             }
 
-            Monkey TestItem(ulong item) {
-                return (item %(ulong)TestDiv == (ulong)0) ? ThrowToTrue : ThrowToFalse;
+            long GetReduction(long item) {
+                return item % BigDiv;
+
+                //return item - ((item / TestDiv -2) * TestDiv);
+            }
+
+            Monkey TestItem(long item, out long itemResult) {
+                //if (item > MAX_NUM) { print($"THIS IS TOO BIG = {item} reduce = {GetReduction(item)}"); }
+                itemResult = (item > MAX_NUM) ? GetReduction(item) : item;                
+                return (itemResult % (long)TestDiv == (long)0) ? ThrowToTrue : ThrowToFalse;
             }
 
             public void Turn() {
@@ -63,7 +77,7 @@ namespace CodeTAF
                 for (int i = 0; i < Items.Count; i++) {
                     Inspects++;
                     Items[i] = Operation(Items[i]);
-                    ThrowTo(TestItem(Items[i]), Items[i]);                    
+                    ThrowTo(TestItem(Items[i], out long updateItem), updateItem);                    
                 }
                 //in theory all items are tossed
                 Items.Clear();
@@ -87,17 +101,17 @@ namespace CodeTAF
             //int the Monkeys aka manual parse the input
             List<Monkey> monkeys = new List<Monkey>();
 
-            monkeys.Add(new Monkey(23, 2, 3, new List<ulong> { 79, 98 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry * 19); };
+            monkeys.Add(new Monkey(23, 2, 3, new List<long> { 79, 98 }));
+            monkeys[^1].Operation = (long worry) => { return (worry * 19); };
 
-            monkeys.Add(new Monkey(19, 2, 0, new List<ulong> { 54, 65, 75, 74 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry + 6); };
+            monkeys.Add(new Monkey(19, 2, 0, new List<long> { 54, 65, 75, 74 }));
+            monkeys[^1].Operation = (long worry) => { return (worry + 6); };
 
-            monkeys.Add(new Monkey(13, 1, 3, new List<ulong> { 79, 60, 97 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry * worry); };
+            monkeys.Add(new Monkey(13, 1, 3, new List<long> { 79, 60, 97 }));
+            monkeys[^1].Operation = (long worry) => { return (worry * worry); };
 
-            monkeys.Add(new Monkey(17, 0, 1, new List<ulong> { 74 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry + 3); };
+            monkeys.Add(new Monkey(17, 0, 1, new List<long> { 74 }));
+            monkeys[^1].Operation = (long worry) => { return (worry + 3); };
 
 
             foreach (Monkey monkey in monkeys) {
@@ -108,32 +122,32 @@ namespace CodeTAF
         }
 
         List<Monkey> InitReal() {
-            //ulong the Monkeys aka manual parse the input
+            //long the Monkeys aka manual parse the input
             List<Monkey> monkeys = new List<Monkey>();
             //0
-            monkeys.Add(new Monkey(19, 5, 3, new List<ulong> { 93, 98 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry * 17); };
+            monkeys.Add(new Monkey(19, 5, 3, new List<long> { 93, 98 }));
+            monkeys[^1].Operation = (long worry) => { return (worry * 17); };
             //1
-            monkeys.Add(new Monkey(13, 7, 6, new List<ulong> { 95, 72, 98, 82, 86 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry + 5); };
+            monkeys.Add(new Monkey(13, 7, 6, new List<long> { 95, 72, 98, 82, 86 }));
+            monkeys[^1].Operation = (long worry) => { return (worry + 5); };
             //2
-            monkeys.Add(new Monkey(5, 3, 0, new List<ulong> { 85, 62, 82, 86, 70, 65, 83, 76 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry + 8); };
+            monkeys.Add(new Monkey(5, 3, 0, new List<long> { 85, 62, 82, 86, 70, 65, 83, 76 }));
+            monkeys[^1].Operation = (long worry) => { return (worry + 8); };
             //3
-            monkeys.Add(new Monkey(7, 4, 5, new List<ulong> { 86, 70, 71, 56 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry + 1); };
+            monkeys.Add(new Monkey(7, 4, 5, new List<long> { 86, 70, 71, 56 }));
+            monkeys[^1].Operation = (long worry) => { return (worry + 1); };
             //4
-            monkeys.Add(new Monkey(17, 1, 6, new List<ulong> { 77, 71, 86, 52, 81, 67 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry + 4); };
+            monkeys.Add(new Monkey(17, 1, 6, new List<long> { 77, 71, 86, 52, 81, 67 }));
+            monkeys[^1].Operation = (long worry) => { return (worry + 4); };
             //5
-            monkeys.Add(new Monkey(2, 1, 4, new List<ulong> { 89, 87, 60, 78, 54, 77, 98 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry * 7); };
+            monkeys.Add(new Monkey(2, 1, 4, new List<long> { 89, 87, 60, 78, 54, 77, 98 }));
+            monkeys[^1].Operation = (long worry) => { return (worry * 7); };
             //6
-            monkeys.Add(new Monkey(3, 7, 2, new List<ulong> { 69, 65, 63 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry + 6); };
+            monkeys.Add(new Monkey(3, 7, 2, new List<long> { 69, 65, 63 }));
+            monkeys[^1].Operation = (long worry) => { return (worry + 6); };
             //7
-            monkeys.Add(new Monkey(11, 0, 2, new List<ulong> { 89 }));
-            monkeys[^1].Operation = (ulong worry) => { return (worry * worry); };
+            monkeys.Add(new Monkey(11, 0, 2, new List<long> { 89 }));
+            monkeys[^1].Operation = (long worry) => { return (worry * worry); };
 
             foreach (Monkey monkey in monkeys) {
                 monkey.SetThrow(monkeys[monkey.TrueMNum], monkeys[monkey.FalseMNum]);
@@ -146,14 +160,14 @@ namespace CodeTAF
         void part1() {
 
             //int the Monkeys aka manual parse the input
-            List<Monkey> monkeys = InitExample();           
+            List<Monkey> monkeys = InitReal();           
 
-            const int TARGET_ROUNDS = 20;
+            const int TARGET_ROUNDS = 10000;
 
             for (int i = 1; i <= TARGET_ROUNDS; i++ ) {
                 for (int mNum = 0; mNum < monkeys.Count; mNum++ ) {
-                    print(i);
-                    if (i == 19) { print(monkeys[mNum].GetItems()); }
+                    //print(i);
+                    //if (i == 19) { print(monkeys[mNum].GetItems()); }
                     monkeys[mNum].Turn();
                 }
             }
@@ -163,12 +177,12 @@ namespace CodeTAF
                 print($"Monkey {i}: {monkeys[i].GetItems()}");
             }
 
-            ulong highestActivity = 0;
-            ulong secondActivity = 0;
+            long highestActivity = 0;
+            long secondActivity = 0;
 
             print("All Activity numbers:");
             for (int i = 0; i < monkeys.Count; i++) {
-                ulong curInspects = (ulong)monkeys[i].Inspects;
+                long curInspects = (long)monkeys[i].Inspects;
                 print($"Monkey {i}: {curInspects}");
 
                 if (curInspects > highestActivity) {
@@ -181,7 +195,6 @@ namespace CodeTAF
             }
 
             print($"Highest = {highestActivity}, 2ndHighest = {secondActivity}, Result = {highestActivity*secondActivity}");
-
 
         }
 
