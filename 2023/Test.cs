7b031b3151615eadce3824766b8301b49bc4ec62
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2023-12-16 00:43:30 -0800

    Started AoC Lib
    Started to make a lib for AoC of more common helpful methods that I have had to commonly use.

---
+++ 2023/Test.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeTAF
+{
+    public class Test : MonoBehaviour
+    {
+        [SerializeField]
+        private bool partTwo = false;
+        [SerializeField]
+        private bool useTestInput = true;
+        [SerializeField]
+        private bool run = false;
+        private string input;
+
+        void part1() {
+            AocLib.TestPrint(input);
+
+
+        }
+
+        void part2() {
+
+        }
+
+        void Update() {
+            if (run) {
+                run = false;
+                Debug.Log("========================================================================");
+
+                if (useTestInput) { input = InputTest(); }
+                else { input = Input(); }
+
+                var startTime = System.DateTime.Now;
+                if (partTwo) { part2(); }
+                else { part1(); }
+                print($"Took {System.DateTime.Now - startTime} to complete.");
+            }
+        }
+
+
+        string InputTest() {
+            return
+@"replaceTestInput";
+        }
+
+        string Input() {
+            return
+@"replaceInput";
+        }
+
+
+    }
+}
---
+++ Aoc Lib.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeTAF
+{
+    public class AocLib : MonoBehaviour
+    {
+        
+        public static void TestPrint(string house) {
+
+            print($"test print = {house}");
+            //Debug.Log(house);
+        }
+        
+        //takes string of even grid of char and put them into a 2d array of char.
+        //can be reversed vertically if needed to shift the 0,0 point to the bottom left instead of the the top left.        
+        public static char[,] ParseSimpleCharMap(string input, bool reverseMap = false) {
+            string[] mapLines = input.Split("\r\n");
+            if (reverseMap) { Array.Reverse(mapLines); }            
+            char[,] output = new char[mapLines[0].Length, mapLines.Length];
+
+            for (int line = 0; line < mapLines.Length; line++) {
+                for (int col = 0; col < mapLines[line].Length; col++) {
+                    output[col, line] = mapLines[line][col];
+                }
+            }
+            return output;
+        }
+
+        //prints to console the 2d array. can be reversed vertically and with tab spacing or now.
+        public static void Print2d<T>(T[,] array2D, bool withTab = true, bool reverseMap = false) {
+            //bool reverseMap = true;
+
+            if (reverseMap) {
+                for (int row = array2D.GetLength(0) - 1; row >= 0; row--) {
+                    string textRow = "";
+                    for (int col = 0; col < array2D.GetLength(1); col++) {
+                        if (withTab) { textRow += "\t"; }
+                        textRow += array2D[col, row];
+                    }
+                    print(textRow);
+                }
+                return;
+            }
+
+            for (int row = 0; row < array2D.GetLength(0); row++) {
+                string textRow = "";
+                for (int col = 0; col < array2D.GetLength(1); col++) {
+                    if (withTab) { textRow += "\t"; }
+                    textRow += array2D[col, row];
+                }
+                print(textRow);
+            }
+        }
+
+        //set all values of an 2d array to the target value. IE: all of bool[,] bob = false;
+        public static T[,] SetAllValues<T>(T[,] inputMap, T targetValue) {
+
+            T[,] outputMap = new T[inputMap.GetLength(0), inputMap.GetLength(1)];
+
+            for (int col = 0; col < inputMap.GetLength(0); col++) {
+                for (int row = 0; row < inputMap.GetLength(1); row++) {
+                    outputMap[col, row] = targetValue;
+                }
+            }
+
+            return outputMap;
+        }
+
+        //Parse all numbers in a string array and return int array. 
+        //may not need due to other methods to do this. 
+        public static int[] GetIntNumbers(string[] numList) {
+            List<int> numbers = new List<int>();
+            
+            foreach (string num in numList) {
+                if (int.TryParse(num, out int newNum)) {
+                    numbers.Add(newNum);
+                }
+            }
+            return numbers.ToArray();
+        }
+
+
+
+    }
+}
