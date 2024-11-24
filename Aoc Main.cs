d970f39151ac60784e438e4cd420d9fcc4efa9d8
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2024-11-24 05:42:07 -0800

    changed main template
    Removed hard coding the input into each script.
    
    Now reads .txt files from inputs folder in the same dir as the script.
    
    Note: The class name needs to end with the two digit day of the AOC.
    
    Note: The inputs should be named the two digit day followed by either "real" or "test" IE: 03test or 03real

--- Aoc Main.cs
+++ Aoc Main.cs
@@ -1,4 +1,8 @@
+using System;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+
 
 namespace CodeTAF
 {
@@ -12,41 +16,72 @@ namespace CodeTAF
         private bool run = false;
         private string input;
 
-        void part1() {
+        private string inputFolderName = "inputs";
+
+
+        string rootPath {
+            get {
+                var g = AssetDatabase.FindAssets($"t:Script {GetType().Name}");
+                return AssetDatabase.GUIDToAssetPath(g[0]);
+            }
+        }
+        string inputPath {
+            get {
+                return $"{rootPath[..^(GetType().Name.Length + 3)]}{inputFolderName}/";
+            }
+        }
+        string day {
+            get {
+                return GetType().Name[^2..];
+            }
+        }
+        string testInput {
+            get {
+                string filePath = $"{inputPath}{day}test.txt";
+
+                if (!File.Exists(filePath)) {
+                    Debug.LogError($"NO input file found @ {filePath}");
+                    return null;
+                }
+                return File.ReadAllText(filePath);
+            }
+        }
+        string realInput {
+            get {
+                string filePath = $"{inputPath}{day}real.txt";
+
+                if (!File.Exists(filePath)) {
+                    Debug.LogError($"NO input file found @ {filePath}");
+                    return null;
+                }
+                return File.ReadAllText(filePath);
+            }
+        }
+
+        void part1() {            
+
 
         }
 
         void part2() {
+            
 
         }
-        
-        void Update()
-        {
+
+        void Update() {
             if (run) {
                 run = false;
                 Debug.Log("========================================================================");
 
-                if (useTestInput) { input = InputTest(); }
-                else { input = Input(); }
+                input = useTestInput ? testInput : realInput;
 
                 var startTime = System.DateTime.Now;
+
                 if (partTwo) { part2(); }
-                else { part1(); }                
+                else { part1(); }
                 print($"Took {System.DateTime.Now - startTime} to complete.");
             }
         }
-
-
-        string InputTest() {
-            return 
-@"replace";
-        }
-
-        string Input() {
-            return 
-@"replace";
-        }
-
-
     }
 }
+
