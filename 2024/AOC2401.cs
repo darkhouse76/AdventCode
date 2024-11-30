50f5dd592344ee7d0ccdc1daeacb8e7a35aa344c
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2024-11-30 08:47:14 -0800

    Prep for 24
    Also added days 1-9 blank inputs txt files in 2024 and 202X folders

--- 2024/AOC2401.cs
+++ 2024/AOC2401.cs
@@ -1,4 +1,8 @@
+using System;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+
 
 namespace CodeTAF
 {
@@ -12,12 +16,55 @@ namespace CodeTAF
         private bool run = false;
         private string input;
 
+        private const string inputFolderName = "inputs";
+
+
+        string RootPath {
+            get {
+                var g = AssetDatabase.FindAssets($"t:Script {GetType().Name}");
+                return AssetDatabase.GUIDToAssetPath(g[0]);
+            }
+        }
+        string InputPath {
+            get {
+                return $"{RootPath[..^(GetType().Name.Length + 3)]}{inputFolderName}/";
+            }
+        }
+        string Day {
+            get {
+                return GetType().Name[^2..];
+            }
+        }
+        string TestInput {
+            get {
+                string filePath = $"{InputPath}{Day}test.txt";
+
+                if (!File.Exists(filePath)) {
+                    Debug.LogError($"NO input file found @ {filePath}");
+                    return null;
+                }
+                return File.ReadAllText(filePath);
+            }
+        }
+        string RealInput {
+            get {
+                string filePath = $"{InputPath}{Day}real.txt";
+
+                if (!File.Exists(filePath)) {
+                    Debug.LogError($"NO input file found @ {filePath}");
+                    return null;
+                }
+                return File.ReadAllText(filePath);
+            }
+        }
 
         void part1() {
+
 
         }
 
         void part2() {
+
 
         }
 
@@ -26,28 +73,15 @@ namespace CodeTAF
                 run = false;
                 Debug.Log("========================================================================");
 
-                if (useTestInput) { input = InputTest(); }
-                else { input = Input(); }
+                input = useTestInput ? TestInput : RealInput;
 
                 var startTime = System.DateTime.Now;
+
                 if (partTwo) { part2(); }
                 else { part1(); }
                 print($"Took {System.DateTime.Now - startTime} to complete.");
             }
         }
-
-
-        string InputTest() {
-            return
-@"replace";
-        }
-
-        string Input() {
-            return
-@"replace";
-        }
-
-
     }
 }
 
