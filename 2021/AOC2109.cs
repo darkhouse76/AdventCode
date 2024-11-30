3d58efb3662bf441d0eab04ec7baba0392dca2e3
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2024-11-30 06:55:09 -0800

    21 day 9 part 1 complete
    not first try.
    
    I forget to check if all adj heights were actually higher than the selected point rather than just equal.

--- 2021/AOC2109.cs
+++ 2021/AOC2109.cs
@@ -59,7 +59,47 @@ namespace CodeTAF
         }
 
         void part1() {
-             
+            string[] heightRows = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+            char[,] heightmap = AocLib.ParseSimpleCharMap(input);
+            AocLib.Print2d(heightmap, true);
+
+            //print(heightmap[1, 1]);
+
+            (int x, int y)[] directions = new [] { (0,1), (1,0), (0,-1), (-1,0) };
+
+            (int x, int y) maxSize = (heightmap.GetLength(0),  heightmap.GetLength(1));
+            int riskLevel = 0;            
+
+            for (int col = 0; col < maxSize.x; col++) {
+                for (int row = 0; row < maxSize.y; row++) {
+                    int selectedHeight = int.Parse( heightmap[col, row].ToString() );
+                    bool isLowPoint = true;
+                    int equalCount = 0;
+                    int checkedDir = 0;
+                    foreach (var dir in directions) {                        
+                        (int x, int y) checkDir = (col + dir.x, row + dir.y);
+
+                        if ((checkDir.x < 0 || checkDir.x >= maxSize.x) || (checkDir.y < 0 || checkDir.y >= maxSize.y)) { continue; }
+                        int checkHeight = int.Parse(heightmap[checkDir.x, checkDir.y].ToString());
+                        checkedDir++;
+                        if (selectedHeight > checkHeight) { isLowPoint = false; break; }
+                        if (selectedHeight == checkHeight) { equalCount++; };
+
+                    }
+
+                    //if not ruled out as low point and all checked dir were equal to the selected height then it's a low point. 
+                    if (isLowPoint && equalCount != checkedDir) { 
+                        print($"at ({col},{row}) is low point: {selectedHeight}"); 
+                        riskLevel += selectedHeight + 1; 
+                    }
+
+                }
+            }
+
+            print($"Risk Level for all points: {riskLevel}");
+
+
 
         }
 
