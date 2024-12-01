1465d63e94b79a2d7d18dc066233e801bef4bc85
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2024-11-30 21:35:51 -0800

    24 Day 1 Part 2 Complete
    First Try!!
    
    Not too bad maybe could have done faster. Re-learned Dictionaries a little bit.

--- 2024/AOC2401.cs
+++ 2024/AOC2401.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
@@ -83,7 +84,49 @@ namespace CodeTAF
         }
 
         void part2() {
+            string[] lines = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+            int[] leftList = new int[lines.Length];
+            int[] rightList = new int[lines.Length];
 
+            for (int i = 0; i < lines.Length; i++) {
+                string[] sides = lines[i].Split("   ");
+                leftList[i] = int.Parse(sides[0]);
+                rightList[i] = int.Parse(sides[1]);
+            }
+
+            Array.Sort(rightList);
+
+            Dictionary<int,int> amtInRightList = new Dictionary<int,int>();
+
+            int curCount = 0;
+            int lastNum = rightList[0];
+            for (int i = 0; i < rightList.Length; i++) {
+                //reset if new number
+                if (rightList[i] == lastNum || i == 0 ) { 
+                    curCount++;
+                    continue;
+                }
+
+                amtInRightList.Add(lastNum, curCount);
+                //to count the cur new number
+                curCount = 1;
+                lastNum = rightList[i];
+            }
+            //catch last number
+            amtInRightList.Add(lastNum, curCount);
+
+            foreach (KeyValuePair<int,int> pair in amtInRightList) {
+                print($"{pair.Key}: x{pair.Value}");
+            }
+
+            int totalSimilarity = 0;
+            for (int i = 0; i <leftList.Length; i++) {
+                if (amtInRightList.TryGetValue(leftList[i], out int amount)) {
+                    totalSimilarity += (leftList[i] * amount);
+                }
+            }
+
+            print($"Total Similarity: {totalSimilarity}");
 
         }
 
