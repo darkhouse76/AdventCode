60bf731331233a0eeb9d67447e8edad78c2b4976
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2024-12-07 07:57:17 -0800

    24 Day 6 Part 1 Attempt
    tried answer of 5108. It was too high of an answer.
    
    Not to sure on what happen. going to look for edge cases

--- 2024/AOC2406.cs
+++ 2024/AOC2406.cs
@@ -3,9 +3,8 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-
-namespace CodeTAF
-{
+namespace CodeTAF{
+    
     public class AOC2406 : MonoBehaviour
     {
         [SerializeField]
@@ -76,16 +75,66 @@ namespace CodeTAF
 
         /////////////////////////////////////////////////////////////////
         /// Everything above is for unity and getting the input files ///
-        /////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////       
+
+        
+        char[,] labMap;
+        (int x, int y) maxSize;
+        bool[,] walkedOn;
+        (int x, int y) curPos;
+        (int x, int y) curDir;
+        int totalWalkAmt = 0;
+
+        
+        //if I didn't know the start dir...but i do
+        (int x, int y) getStartPos(out (int x, int y) curDir) {
+            var startIndicators = new char[] { '^', '<', 'v', '>' };
+            curDir = default;
+
+            for (int i = 0; i < startIndicators.Length; i++) {
+                
+                if (AocLib.Map.TryFind(labMap, startIndicators[i], out var startPos)) {
+                    curDir = AocLib.Map.Directions[i];
+                    return startPos; 
+                }                
+            }
+            return default;
+        }
+        
+        
+        bool walk() {
+            var targetPos = AocLib.Map.MoveForward(curPos, curDir);
+            if (!AocLib.Map.IsInBounds(targetPos, maxSize)) {
+                return false;
+            }
+
+            if (labMap[targetPos.x,targetPos.y] == '#') { 
+                curDir = AocLib.Map.TurnRight(curDir); 
+                walk(); 
+            } else {
+                curPos = targetPos;
+                if (!walkedOn[curPos.x,curPos.y]) { walkedOn[curPos.x,curPos.y] = true; totalWalkAmt++; }
+            }
+            return true;            
+        }
+
 
         void part1() {
+            labMap = AocLib.ParseSimpleCharMap(input);
+            maxSize = (labMap.GetLength(0), labMap.GetLength(1));
+            walkedOn = AocLib.SetAllValues(new bool[maxSize.x, maxSize.y], false);
+            curPos = AocLib.Map.Find(labMap,'^');
+            curDir = AocLib.Map.Directions[AocLib.Map.UP];
+            
+            while (walk()) { continue; }
 
+            print($"Guard walked total of {totalWalkAmt} distinct positions");
 
         }
 
         void part2() {
-
-
+            labMap = AocLib.ParseSimpleCharMap(input);
+            maxSize = (labMap.GetLength(0), labMap.GetLength(1));
         }
 
 
