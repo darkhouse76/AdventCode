f641256c84ba0272a80954e22d9f47b868657fa5
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2024-12-14 14:14:41 -0800

    24 Day 14 Part 1 Complete
    My first attempt probably would have worked.... if I realized my regex was messed up for the real input but not the test input. When you are missing over half the robots, shouldn't be surpising that your answer is to low......

--- 2024/AOC2414.cs
+++ 2024/AOC2414.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Xml.Schema;
 using UnityEditor;
 using UnityEngine;
 
@@ -88,26 +89,25 @@ namespace CodeTAF
         (int minX, int minY, int maxX, int maxY)[] quadrantBounds;
 
         class robot {
-            (int x, int y) startPos;
-            (int x, int y) curVector;            
 
+            static public int TotalRobots = 0;
+            (int x, int y) startPos;
+            (int x, int y) curVector;   
 
             public robot((int x, int y) StartPos, (int x, int y) CurVector) {
                 this.startPos = StartPos;
-                this.curVector = CurVector;                
+                this.curVector = CurVector;
+                TotalRobots++;
             }
 
             public (int x, int y) getFuturePos(int seconds) {
-                (int x, int y) nonWrapPos = ((curVector.x * seconds) + startPos.x, (curVector.y * seconds) + startPos.y);
 
-                int newX = Math.Abs(nonWrapPos.x) - (Math.Abs(nonWrapPos.x / maxSize.x) * maxSize.x);
-                newX = nonWrapPos.x < 0 ? (maxSize.x - newX) : newX;
-                int newY = Math.Abs(nonWrapPos.y) - (Math.Abs(nonWrapPos.y / maxSize.y) * maxSize.y);
-                newY = nonWrapPos.y < 0 ? (maxSize.y - newY) : newY;
-
-                return (newX, newY);
-            }
-
+                int newX = ((curVector.x * seconds) + startPos.x) % maxSize.x;
+                newX = (newX < 0) ? (maxSize.x + newX) : newX;
+                int newY = ((curVector.y * seconds) + startPos.y) % maxSize.y;
+                newY = (newY < 0) ? (maxSize.y + newY) : newY;
+                return (newX, newY);                
+            } 
         }
 
         int getQuadrant((int x, int y) pos) {
@@ -123,9 +123,11 @@ namespace CodeTAF
 
 
         void part1() {
-            maxSize = useTestInput ? (11, 7) : (100, 102);
+            maxSize = useTestInput ? (11, 7) : (101, 103);
             int wideDiv = (maxSize.x / 2);
             int tallDiv = (maxSize.y / 2);
+
+            //int[,] visMap = new int[maxSize.x, maxSize.y];
 
             quadrantBounds = new (int minX, int minY, int maxX, int maxY)[] {
                 //top left
@@ -139,22 +141,29 @@ namespace CodeTAF
             };
 
             //look at the cool regex lol
-            var robotInputs = Regex.Matches(input, @"p=(?<posX>\d+|-\d),(?<posY>\d+|-\d) v=(?<velX>\d+|-\d),(?<velY>\d+|-\d)");
+            var robotInputs = Regex.Matches(input, @"p=(?<posX>\d+|-\d+),(?<posY>\d+|-\d+) v=(?<velX>\d+|-\d+),(?<velY>\d+|-\d+)");
             int[] botCountPerQuadrant = new int[4];
             List<robot> allRobots = new();
+            robot.TotalRobots = 0;
 
             foreach( Match robotInput in robotInputs ) {
                 (int x, int y) pos = ( int.Parse(robotInput.Groups["posX"].Value), int.Parse(robotInput.Groups["posY"].Value) );
                 (int x, int y) vel = ( int.Parse(robotInput.Groups["velX"].Value), int.Parse(robotInput.Groups["velY"].Value) );
                 allRobots.Add(new robot(pos, vel));
-            }
-            
-            foreach( var bot in allRobots) {                
-                int quadrant = getQuadrant(bot.getFuturePos(secondsInFuture));                
+            } 
+
+            foreach( var bot in allRobots) {
+                var futurePos = bot.getFuturePos(secondsInFuture);                
+                int quadrant = getQuadrant(futurePos);                
                 if (quadrant >= 0 ) { botCountPerQuadrant[quadrant]++; }
             }
+            
+            long safetyFactor = 1;
+            foreach ( var botCount in botCountPerQuadrant) {
+                safetyFactor *= botCount;
+            }
 
-            int safetyFactor = botCountPerQuadrant.Aggregate(1, (total, next) => total *= next);
+            //long safetyFactor = botCountPerQuadrant.Aggregate(1, (total, next) => total *= next);
 
             print($"The safety factor after {secondsInFuture} seconds = {safetyFactor}");
         }
