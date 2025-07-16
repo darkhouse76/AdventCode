87a52b99505a0af69ed953adc64c6f50b9a8babe
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2025-07-16 14:21:34 -0700

    Create AOC2415.cs

---
+++ 2024/AOC2415.cs
@@ -0,0 +1,95 @@
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace CodeTAF
+{
+    public class AOC2415 : MonoBehaviour
+    {
+        [SerializeField]
+        private bool partTwo = false;
+        [SerializeField]
+        private bool useTestInput = true;
+        [SerializeField]
+        private bool run = false;
+        private string input;
+
+        private const string inputFolderName = "inputs";
+
+
+        string RootPath {
+            get {
+                var g = AssetDatabase.FindAssets($"t:Script {GetType().Name}");
+                return AssetDatabase.GUIDToAssetPath(g[0]);
+            }
+        }
+        string InputPath {
+            get {
+                return $"{RootPath[..^(GetType().Name.Length + 3)]}{inputFolderName}/";
+            }
+        }
+        string Day {
+            get {
+                return GetType().Name[^2..];
+            }
+        }
+        string TestInput {
+            get {
+                string filePath = $"{InputPath}{Day}test";
+                filePath = (File.Exists(filePath + "2.txt")) ? $"{filePath}2.txt" : $"{filePath}.txt";
+
+                if (!File.Exists(filePath)) {
+                    Debug.LogError($"NO input file found @ {filePath}");
+                    return null;
+                }
+                return File.ReadAllText(filePath);
+            }
+        }
+        string RealInput {
+            get {
+                string filePath = $"{InputPath}{Day}real.txt";
+
+                if (!File.Exists(filePath)) {
+                    Debug.LogError($"NO input file found @ {filePath}");
+                    return null;
+                }
+                return File.ReadAllText(filePath);
+            }
+        }
+
+        void Update() {
+            if (run) {
+                run = false;
+                Debug.Log("========================================================================");
+
+                input = useTestInput ? TestInput : RealInput;
+
+                var startTime = System.DateTime.Now;
+
+                if (partTwo) { part2(); }
+                else { part1(); }
+                print($"Took {System.DateTime.Now - startTime} to complete.");
+            }
+        }
+
+        /////////////////////////////////////////////////////////////////
+        /// Everything above is for unity and getting the input files ///
+        /////////////////////////////////////////////////////////////////
+
+        void part1() {
+
+
+        }
+
+        void part2() {
+
+
+        }
+
+
+
+    }
+}
+
